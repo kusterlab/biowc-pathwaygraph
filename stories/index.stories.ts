b3dc4a0bec7ad7b6f4b7778bd6f7b07c33df5ee0
Julian Müller <julian2.mueller@tum.de>
2023-07-14 12:42:19 +0200

    Add most stories (stories for public functions still missing)

--- stories/index.stories.ts
+++ stories/index.stories.ts
@@ -1,15 +1,17 @@
 import { html, TemplateResult } from 'lit';
 import '../src/biowc-pathwaygraph.js';
-import Story1Fixture from '../test/fixtures/Story1Fixture.js';
+import StoryFixtures from '../test/fixtures/StoryFixtures.js';
 
 export default {
   title: 'BiowcPathwaygraph',
   component: 'biowc-pathwaygraph',
   argTypes: {
+    storyTitle: { control: 'null' },
     pathwayMetaData: { control: 'object' },
     graphdataSkeleton: { control: 'object' },
-    ptmInputList: { control: 'object' },
+    ptmInputList: { control: 'array' },
     fullProteomeInputList: { control: 'object' },
+    hue: { control: 'text' },
   },
 };
 
@@ -26,6 +28,7 @@ interface ArgTypes {
   graphdataSkeleton?: object;
   ptmInputList?: object;
   fullProteomeInputList?: object;
+  hue: string;
 }
 
 const Template: Story<ArgTypes> = (args: ArgTypes) => html`
@@ -36,22 +39,190 @@ const Template: Story<ArgTypes> = (args: ArgTypes) => html`
     .graphdataSkeleton=${args.graphdataSkeleton}
     .ptmInputList=${args.ptmInputList}
     .fullProteomeInputList=${args.fullProteomeInputList}
+    .hue=${args.hue}
   >
   </biowc-pathwaygraph>
 `;
 
-export const MinimalSkeletonGraph = Template.bind({});
-MinimalSkeletonGraph.args = {
-  storyTitle: 'Minimal Skeleton Graph',
-  storyDescription: 'This is a graph without any PTMs. Just the skeleton.',
-};
-MinimalSkeletonGraph.args.graphdataSkeleton = {
-  nodes: Story1Fixture.pathway1.nodes,
-  links: Story1Fixture.pathway1.links,
+// Simple Skeleton Graph
+// 2 Nodes, one edge
+export const SimpleSkeletonGraph = Template.bind({});
+SimpleSkeletonGraph.args = {
+  graphdataSkeleton: {
+    nodes: StoryFixtures.simpleSkeletonFixture.nodes,
+    links: StoryFixtures.simpleSkeletonFixture.links,
+  },
+  storyTitle: 'A Simple Skeleton Graph',
+  storyDescription:
+    'The skeleton of a graph consists of:' +
+    '- A list of nodes, including their coordinates' +
+    '- A list of links that connect the nodes' +
+    "These two parts of the skeleton are defined under the 'nodes' and 'links' properties of a biowc-pathwaygraph component instance. " +
+    'The graph is then automatically rendered. ' +
+    'The graph is interactive: You can drag the nodes around and the edges will follow.' +
+    'Click on a node to select it. Press CTRL to select multiple nodes. Click the canvas to clear the selection.',
 };
 
-export const MinimalPTMGraph = Template.bind({});
-MinimalPTMGraph.args = {
-  ...MinimalSkeletonGraph.args,
-  ptmInputList: Story1Fixture.pathway1.ptmInputList,
+// Node types
+export const NodeTypes = Template.bind({});
+NodeTypes.args = {
+  graphdataSkeleton: {
+    nodes: StoryFixtures.nodeTypesFixture.nodes,
+    links: StoryFixtures.nodeTypesFixture.links,
+  },
+  storyTitle: 'Node types',
+  storyDescription:
+    "The type of a node is set using the 'type' property. Possible types are: " +
+    '- gene_protein' +
+    '- compound' +
+    '- pathway' +
+    '- group' +
+    'Group nodes are part of the node list, but they do not have coordinates. ' +
+    'They are drawn as a hull around their members and updated when their members change position.' +
+    "Any node can become part of a group by setting its 'groupId' property to the group's 'nodeId'." +
+    'A link can go directly to a group as well as to its individual members.',
+};
+
+// Alternative names
+export const AlternativeNames = Template.bind({});
+AlternativeNames.args = {
+  graphdataSkeleton: {
+    nodes: StoryFixtures.alternativeNamesFixture.nodes,
+    links: StoryFixtures.alternativeNamesFixture.links,
+  },
+  storyTitle: 'Alternative Names',
+  storyDescription:
+    "A node of type 'gene_protein' or 'compound' can have alternative names. The name can be changed by right-clicking the node." +
+    "Alternative Names are defined in the 'label' property of a node. They are supplied as a single string, the different names need to be separated by ',' or by ';'." +
+    "For 'gene_protein' nodes, additional alternative names can be supplied as a list of strings in the 'geneNames' property.",
+};
+
+// Edge types
+// Edges on edges
+export const LinkTypes = Template.bind({});
+LinkTypes.args = {
+  graphdataSkeleton: {
+    nodes: StoryFixtures.linkTypesFixture.nodes,
+    links: StoryFixtures.linkTypesFixture.links,
+  },
+  storyTitle: 'Link Types',
+  storyDescription:
+    'Links can also have different types:' +
+    "- 'activation'" +
+    "- 'binding/association'" +
+    "- 'inhibition'" +
+    "- 'indirect effect' " +
+    "Links without a type are automatically classified as 'other'. " +
+    "The type of a link is defined in its 'types' property (for compatibility reasons this is a list, " +
+    'but you will usually only supply a single type there). ' +
+    'A link can also start or end on another link. Just specify the id of the other link as sourceId or targetId.',
+};
+
+// Simple PTM Graph
+export const SimplePTMGraph = Template.bind({});
+SimplePTMGraph.args = {
+  graphdataSkeleton: {
+    nodes: StoryFixtures.simplePTMGraphFixture.nodes,
+    links: StoryFixtures.simplePTMGraphFixture.links,
+  },
+  ptmInputList: StoryFixtures.simplePTMGraphFixture.ptmInputList,
+  storyTitle: 'Simple PTM Graph',
+  storyDescription:
+    "PTMs are added to the skeleton graph using the 'ptmInputList' property " +
+    "of a 'biowc-pathwaygraph' component instance. " +
+    "An entry of the 'ptmInputList' must contain a 'geneNames' or a 'uniprotAccs' property," +
+    'by which it can be matched to a node in the skeleton graph. Additionally it must ' +
+    "contain a 'regulation' property (possible values are 'up', 'down', 'not'). " +
+    'The regulations of a node are initially summarized by node category. You can expand each category by double-clicking it. ' +
+    "PTM Nodes have two simulated forces. One keeps them close to their 'gene_protein' node, the other keeps them from overlapping with one another. " +
+    'Try dragging them around!',
+};
+
+// PTM Graph with more Details (Tooltip - it only appears when there are details but that is ok)
+export const PTMGraphWithDetails = Template.bind({});
+PTMGraphWithDetails.args = {
+  ...SimplePTMGraph.args,
+  ptmInputList: StoryFixtures.ptmGraphWithDetailsFixture.ptmInputList,
+  storyTitle: 'PTM Graph with Details',
+  storyDescription:
+    "In the `details` property of `ptmInputList`'s elements, " +
+    'you can but arbitrary key-value pairs with information.' +
+    'This information is then displayed as a tooltip. ' +
+    'Expand one of the PTM nodes and hover over it to see it!',
+};
+
+// PTM Graph with FPs
+export const ProteinExpressionGraph = Template.bind({});
+ProteinExpressionGraph.args = {
+  graphdataSkeleton: {
+    nodes: StoryFixtures.proteinExpressionFixture.nodes,
+    links: StoryFixtures.proteinExpressionFixture.links,
+  },
+  fullProteomeInputList:
+    StoryFixtures.proteinExpressionFixture.fullProteomeInputList,
+  storyTitle: 'Protein Expression Graph',
+  storyDescription:
+    "You can also supply protein expression ('full proteome') data " +
+    'using the `fullProteomeInputList` property. ' +
+    "The 'gene_protein' nodes will be colored accordingly. " +
+    "Note that nodes with a 'not' regulation (e.g. Protein C) are distinguished from nodes with no regulation (e.g. Protein E). " +
+    'If multiple entries map to the same node, their details are concatenated. ' +
+    'If both up- and down-regulations map to the same node, the node will be streaked red and blue. ',
+};
+
+// PTM Graph with Fold Change
+export const ColoringNodesByFoldChange = Template.bind({});
+ColoringNodesByFoldChange.args = {
+  ...PTMGraphWithDetails.args,
+  hue: 'foldchange',
+  storyTitle: 'Coloring Nodes By Fold Change',
+  storyDescription:
+    'If the details of the `ptmInputList` contain fold changes or log fold changes, ' +
+    "you can change the color scheme of the graph by setting the `hue` property to 'foldchange'. " +
+    'The legend will then be extended by a dynamic color bar that ranges from the smallest' +
+    'to the largest value, and each PTM node will be colored accordingly. ' +
+    '(This also works for log fold changes, where downregulations would be negative.)',
+};
+
+// DecryptM Graph with Potency Hue
+export const ColoringNodesByPotency = Template.bind({});
+ColoringNodesByPotency.args = {
+  ...PTMGraphWithDetails.args,
+  hue: 'potency',
+  storyTitle: 'Coloring Nodes By Fold Change (decryptM Data)',
+  storyDescription:
+    'If you have decryptM data, each peptide of the `ptmInputList` ' +
+    "has a '-log(EC50)' value in its details. For this type of data, a third color scheme exists, which " +
+    "you can activate by setting `hue` to 'potency'. The dynamic color bar will then " +
+    "be use the 'viridis' color scale and will range from the lowest to the highest potency " +
+    '(negative log EC50 value).',
+};
+
+// Todo: Demo Public methods (download, collapse (in same story as expand), select downstream)
+const TemplateWithButton: Story<ArgTypes> = (args: ArgTypes) => html`
+  <div>${args.storyTitle}</div>
+  <div>${args.storyDescription}</div>
+  <button
+    id="button"
+    onclick="document.getElementById('pathwaygraph').expandAllPTMNodes()"
+  >
+    Click me!
+  </button>
+  <biowc-pathwaygraph
+    id="pathwaygraph"
+    .pathwayMetaData=${args.pathwayMetaData}
+    .graphdataSkeleton=${args.graphdataSkeleton}
+    .ptmInputList=${args.ptmInputList}
+    .fullProteomeInputList=${args.fullProteomeInputList}
+    .hue=${args.hue}
+  >
+  </biowc-pathwaygraph>
+`;
+
+export const ExpandAllFunction = TemplateWithButton.bind({});
+ExpandAllFunction.args = {
+  ...PTMGraphWithDetails.args,
+  hue: 'potency',
+  storyTitle: 'Blah',
+  storyDescription: 'Blahblah',
 };
---
+++ test/fixtures/StoryFixtures.ts
@@ -0,0 +1,425 @@
+export default {
+  simpleSkeletonFixture: {
+    nodes: [
+      {
+        nodeId: '1',
+        label: 'Protein A',
+        type: 'gene_protein',
+        x: 50,
+        y: 50,
+      },
+      {
+        nodeId: '2',
+        label: 'Protein B',
+        type: 'gene_protein',
+        x: 175,
+        y: 50,
+      },
+    ],
+    links: [
+      {
+        linkId: 'relation-1',
+        sourceId: '1',
+        targetId: '2',
+        types: [],
+      },
+    ],
+  },
+  nodeTypesFixture: {
+    nodes: [
+      {
+        nodeId: '1',
+        label: 'Protein A',
+        type: 'gene_protein',
+        x: 50,
+        y: 50,
+      },
+      {
+        nodeId: '2',
+        label: 'Compound X',
+        type: 'compound',
+        x: 175,
+        y: 50,
+      },
+      {
+        nodeId: '3',
+        label: 'Protein B2',
+        type: 'gene_protein',
+        groupId: '5',
+        x: 175,
+        y: 100,
+      },
+      {
+        nodeId: '4',
+        label: 'Protein B1',
+        type: 'gene_protein',
+        groupId: '5',
+        x: 100,
+        y: 100,
+      },
+      {
+        nodeId: '5',
+        type: 'group',
+      },
+      {
+        nodeId: '6',
+        label: 'Some Other Pathway',
+        type: 'pathway',
+        x: 125,
+        y: 150,
+      },
+    ],
+    links: [
+      {
+        linkId: 'relation-1',
+        sourceId: '1',
+        targetId: '2',
+        types: [],
+      },
+      {
+        linkId: 'relation-2',
+        sourceId: '2',
+        targetId: '5',
+        types: [],
+      },
+      {
+        linkId: 'relation-3',
+        sourceId: '1',
+        targetId: '4',
+        types: [],
+      },
+      {
+        linkId: 'relation-4',
+        sourceId: '5',
+        targetId: '6',
+        types: [],
+      },
+    ],
+  },
+  alternativeNamesFixture: {
+    nodes: [
+      {
+        nodeId: '1',
+        label: 'Protein A,ProtA',
+        type: 'gene_protein',
+        x: 50,
+        y: 50,
+      },
+      {
+        nodeId: '2',
+        label: 'Compound X1;Compound X2',
+        type: 'compound',
+        x: 175,
+        y: 50,
+      },
+      {
+        nodeId: '3',
+        label: 'GeneB',
+        geneNames: ['GeneName1', 'GeneName2'],
+        type: 'gene_protein',
+        x: 175,
+        y: 100,
+      },
+    ],
+    links: [
+      {
+        linkId: 'relation-1',
+        sourceId: '1',
+        targetId: '2',
+        types: [],
+      },
+      {
+        linkId: 'relation-2',
+        sourceId: '2',
+        targetId: '3',
+        types: [],
+      },
+    ],
+  },
+  linkTypesFixture: {
+    nodes: [
+      {
+        nodeId: '1',
+        geneNames: ['Protein A'],
+        type: 'gene_protein',
+        x: 50,
+        y: 50,
+      },
+      {
+        nodeId: '2',
+        geneNames: ['Protein B'],
+        type: 'gene_protein',
+        x: 175,
+        y: 50,
+      },
+      {
+        nodeId: '3',
+        geneNames: ['Protein C'],
+        type: 'gene_protein',
+        x: 175,
+        y: 100,
+      },
+
+      {
+        nodeId: '4',
+        geneNames: ['Protein D'],
+        type: 'gene_protein',
+        x: 100,
+        y: 100,
+      },
+    ],
+    links: [
+      {
+        linkId: 'relation-1',
+        sourceId: '1',
+        targetId: '2',
+        types: ['activation'],
+      },
+      {
+        linkId: 'relation-2',
+        sourceId: '2',
+        targetId: '3',
+        types: ['binding/association'],
+      },
+      {
+        linkId: 'relation-3',
+        sourceId: '3',
+        targetId: '4',
+        types: ['inhibition'],
+      },
+      {
+        linkId: 'relation-4',
+        sourceId: '4',
+        targetId: '1',
+        types: ['indirect effect'],
+      },
+      {
+        linkId: 'relation-5',
+        sourceId: 'relation-1',
+        targetId: 'relation-3',
+        types: [],
+      },
+    ],
+  },
+  simplePTMGraphFixture: {
+    nodes: [
+      {
+        nodeId: '1',
+        geneNames: ['Protein A', 'GeneA'],
+        type: 'gene_protein',
+        x: 50,
+        y: 50,
+      },
+      {
+        nodeId: '2',
+        geneNames: ['Protein B'],
+        uniprotAccs: ['P12345'],
+        type: 'gene_protein',
+        x: 175,
+        y: 50,
+      },
+    ],
+    links: [
+      {
+        linkId: 'relation-1',
+        sourceId: '1',
+        targetId: '2',
+        types: [],
+      },
+    ],
+    ptmInputList: [
+      {
+        geneNames: ['GeneA'],
+        regulation: 'down',
+      },
+      {
+        geneNames: ['Protein A'],
+        regulation: 'down',
+      },
+      {
+        geneNames: ['Protein B'],
+        regulation: 'not',
+      },
+      {
+        uniprotAccs: ['P12345'],
+        regulation: 'up',
+      },
+    ],
+  },
+  ptmGraphWithDetailsFixture: {
+    ptmInputList: [
+      {
+        geneNames: ['GeneA'],
+        regulation: 'down',
+        details: {
+          Sequence: 'TESTPEPT(ph)IDEK',
+          'Fold Change': 0.5,
+          'p Value': 0.0001,
+          '-log(EC50)': 7,
+        },
+      },
+      {
+        geneNames: ['Protein A'],
+        regulation: 'down',
+        details: {
+          Sequence: 'TRY(ph)AGAINK',
+          'Fold Change': 0.1,
+          'p Value': 0.0002,
+          '-log(EC50)': 9,
+        },
+      },
+      {
+        geneNames: ['Protein B'],
+        regulation: 'not',
+        details: {
+          Sequence: 'GILGVIVT(ph)LK',
+          'Fold Change': 0.9,
+          'p Value': 0.0003,
+          '-log(EC50)': 6,
+        },
+      },
+      {
+        uniprotAccs: ['P12345'],
+        regulation: 'up',
+        details: {
+          Sequence: 'VNIPRVT(ph)K',
+          'Fold Change': 4,
+          'p Value': 0.0004,
+          '-log(EC50)': 10,
+        },
+      },
+      {
+        geneNames: ['Protein B'],
+        regulation: 'up',
+        details: {
+          Sequence: 'GENENAMES(ph)K',
+          'Fold Change': 2,
+          'p Value': 0.001,
+          '-log(EC50)': 8,
+        },
+      },
+    ],
+  },
+  proteinExpressionFixture: {
+    nodes: [
+      {
+        nodeId: '1',
+        geneNames: ['Protein A'],
+        type: 'gene_protein',
+        x: 50,
+        y: 50,
+      },
+      {
+        nodeId: '2',
+        geneNames: ['Protein B'],
+        type: 'gene_protein',
+        x: 175,
+        y: 50,
+      },
+      {
+        nodeId: '3',
+        geneNames: ['Protein C'],
+        type: 'gene_protein',
+        x: 175,
+        y: 100,
+      },
+
+      {
+        nodeId: '4',
+        geneNames: ['Protein D'],
+        type: 'gene_protein',
+        x: 100,
+        y: 100,
+      },
+      {
+        nodeId: '5',
+        geneNames: ['Protein E'],
+        type: 'gene_protein',
+        x: 30,
+        y: 100,
+      },
+    ],
+    links: [
+      {
+        linkId: 'relation-1',
+        sourceId: '1',
+        targetId: '2',
+        types: ['activation'],
+      },
+      {
+        linkId: 'relation-2',
+        sourceId: '2',
+        targetId: '3',
+        types: ['binding/association'],
+      },
+      {
+        linkId: 'relation-3',
+        sourceId: '3',
+        targetId: '4',
+        types: ['inhibition'],
+      },
+      {
+        linkId: 'relation-4',
+        sourceId: '4',
+        targetId: '1',
+        types: ['indirect effect'],
+      },
+      {
+        linkId: 'relation-5',
+        sourceId: 'relation-1',
+        targetId: 'relation-3',
+        types: [],
+      },
+      {
+        linkId: 'relation-6',
+        sourceId: '5',
+        targetId: '4',
+        types: [],
+      },
+    ],
+    fullProteomeInputList: [
+      {
+        geneNames: ['Protein A'],
+        regulation: 'down',
+        details: {
+          'Fold Change': 0.1,
+        },
+      },
+      {
+        geneNames: ['Protein B'],
+        regulation: 'up',
+        details: {
+          'Fold Change': 2.5,
+        },
+      },
+      {
+        geneNames: ['Protein B'],
+        regulation: 'up',
+        details: {
+          'Fold Change': 3.0,
+        },
+      },
+      {
+        geneNames: ['Protein D'],
+        regulation: 'down',
+        details: {
+          'Fold Change': 0.25,
+        },
+      },
+      {
+        geneNames: ['Protein D'],
+        regulation: 'up',
+        details: {
+          'Fold Change': 1.5,
+        },
+      },
+      {
+        geneNames: ['Protein C'],
+        regulation: 'not',
+        details: {
+          'Fold Change': 1.01,
+        },
+      },
+    ],
+  },
+};
