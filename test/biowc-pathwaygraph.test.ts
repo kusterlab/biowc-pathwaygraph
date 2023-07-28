3e675eeca3439b00cb82f03fa3926760a8875daa
Julian Müller <julian2.mueller@tum.de>
2023-07-28 13:47:41 +0200

    Rename test

--- test/biowc-pathwaygraph.test.ts
+++ test/biowc-pathwaygraph.test.ts
@@ -4,21 +4,22 @@ import StoryFixtures from './fixtures/StoryFixtures.js';
 import { BiowcPathwaygraph } from '../src/BiowcPathwaygraph.js';
 import '../src/biowc-pathwaygraph.js';
 
+// TODO: a11y audit as separate test, needs to wait for the rendering to finish
+
 describe('BiowcPathwaygraph', () => {
-  it('passes the a11y audit', async () => {
+  it('can be rendered and passes the a11y audit', async () => {
     const el = await fixture<BiowcPathwaygraph>(
-      html`<biowc-pathwaygraph
-        .graphdataSkeleton=${{
+      html` <biowc-pathwaygraph
+        .graphdataSkeleton="${{
           nodes: StoryFixtures.simplePTMGraphFixture.nodes,
           links: StoryFixtures.simplePTMGraphFixture.links,
-        }}
-        .ptmInputList=${StoryFixtures.ptmGraphWithDetailsFixture.ptmInputList}
-        .fpInputList=${StoryFixtures.proteinExpressionFixture
-          .fullProteomeInputList}
-        .hue=${'foldchange'}
+        }}"
+        .ptmInputList="${StoryFixtures.ptmGraphWithDetailsFixture.ptmInputList}"
+        .fpInputList="${StoryFixtures.proteinExpressionFixture
+          .fullProteomeInputList}"
+        .hue="${'foldchange'}"
       ></biowc-pathwaygraph>`
     );
-
     await expect(el).shadowDom.to.be.accessible();
   });
 });
